8665140b242802fb812622185769fa2256f78e38
msartuc <mserdarartuc@gmail.com>
2024-12-22 00:26:36 +0300

    png uploaded

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """ overall class to set up the game behaviour and variable """
@@ -7,9 +8,11 @@ class AlienInvasion:
     def __init__(self):
         """ initialize the game """
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200,800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()   
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
 
     def run_game(self):
         """ starts the main loop for the game """
@@ -18,8 +21,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                
+            
+            self.screen.fill(self.settings.bg_color)   
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # make a game instance and run it
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """ class to keep all settings"""
+
+    def __init__(self):
+        """ initialize settings """
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
