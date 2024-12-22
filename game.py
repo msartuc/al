33b6e16cf084e6a66ffe6700d018c2a38c2c6048
msartuc <mserdarartuc@gmail.com>
2024-12-22 23:27:15 +0300

    button refresing + working

--- game.py
+++ game.py
@@ -53,6 +53,18 @@ class AlienInvasion:
                     self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    self._check_play_button(mouse_pos)
+
+    def _check_play_button(self,mouse_pos):
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.reset_stats()
+            self.game_active = True
+            self.bullets.empty()
+            self.aliens.empty()
+            self._create_fleet()
+            self.ship.center_ship()
     
     def _check_keydown_events(self,event):
         if event.key == pygame.K_RIGHT:
